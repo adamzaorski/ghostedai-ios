07c71d7181442b559b881754ba6f128ebacbdcfe
adamzaorski <142094232+adamzaorski@users.noreply.github.com>
2025-10-05 20:01:40 +0200

    Initial Commit

---
+++ GhostedAI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GhostedAI
+//
+//  Created by Adam Zaorski on 10/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GhostedAITests/GhostedAITests.swift
@@ -0,0 +1,17 @@
+//
+//  GhostedAITests.swift
+//  GhostedAITests
+//
+//  Created by Adam Zaorski on 10/5/25.
+//
+
+import Testing
+@testable import GhostedAI
+
+struct GhostedAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
