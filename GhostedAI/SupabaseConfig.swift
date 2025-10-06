14070daac8ffb74d082fa664e93b131014400d54
adamzaorski <142094232+adamzaorski@users.noreply.github.com>
2025-10-06 14:55:53 +0200

    Gitignore Supabase credentials

---
+++ GhostedAI/SupabaseConfig.swift
@@ -0,0 +1,14 @@
+//
+//  SupabaseConfig 2.swift
+//  GhostedAI
+//
+//  Created by Adam Zaorski on 10/6/25.
+//
+
+
+import Foundation
+
+enum SupabaseConfig {
+    static let url = "https://bldjbqpuqrvjfcnpsunv.supabase.co"
+    static let anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZGpicXB1cXJ2amZjbnBzdW52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTA0NjEsImV4cCI6MjA3NTMyNjQ2MX0.BRLj8t0SYzjNUwcx31_g2Q_XKIbOxl_h-PQM5blXcFo"
+}
